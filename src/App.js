62bd5b5caabcf4403276aa6d132fcb7decb7b7b8
Aroyan <43630681+aroyan@users.noreply.github.com>
2022-11-28 01:43:09 +0800

    chore: add new logo, favicon, and error page (#5)
    * feat: add landing page
    
    * fix: edit default title and description in index.html
    
    * feat: add README.md
    
    * chore: remove unsued files
    
    * chore: update logo and favicon
    
    * feat: add error page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
+
+import Error from "./pages/Error";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
-        <Routes>
-        <Route index element={<Home />}></Route> 
-    </Routes>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/*" element={<Error />} />
+      </Routes>
     </Router>
   );
 }
