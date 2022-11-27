f8a03215b54a3e09f71dc8d55cfa316a9de009ed
Aroyan <43630681+aroyan@users.noreply.github.com>
2022-11-27 18:03:24 +0000

    chore: create reusable component for feature card

--- src/pages/Home/FeatureSection.jsx
+++ src/pages/Home/FeatureSection.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React from 'react';
+
+const FEATURES_DATA = [
+  {
+    icon: <WalletIcon />,
+    title: 'Various Payment Methods',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, adipisci!',
+  },
+  {
+    icon: <DollarIcon />,
+    title: 'No Hidden Fee',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+  },
+  {
+    icon: <RefundIcon />,
+    title: 'Refundable',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis molestiae.',
+  },
+];
 
 function FeatureSection() {
   return (
-    <div className="container mx-auto my-8">
+    <section className="container mx-auto my-8">
       <div className="xl:h-[700px] flex flex-col justify-center">
         <div className="mb-8 text-center">
           <h2 className="text-3xl font-bold my-8">Why Terbang Tinggi?</h2>
@@ -12,44 +32,23 @@ function FeatureSection() {
           </p>
         </div>
         <div className="flex justify-center xl:justify-between mx-4 flex-wrap items-center gap-4 flex-col xl:flex-row">
-          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md ">
-            <WalletIcon />
-            <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900">
-              Various Payment Methods
-            </h5>
-            <p className="mb-3 font-normal text-gray-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum,
-              adipisci!
-            </p>
-          </div>
-          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md ">
-            <DollarIcon />
-            <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-">
-              No Hidden Fee
-            </h5>
-            <p className="mb-3 font-normal text-gray-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing.
-            </p>
-          </div>
-          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md">
-            <RefundIcon />
-            <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900">
-              Refundable<sup className="text-red-500">*</sup>
-            </h5>
-            <p className="mb-3 font-normal text-gray-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Perspiciatis molestiae
-            </p>
-          </div>
+          {FEATURES_DATA.map((item) => (
+            <FeatureCard
+              key={item.title}
+              icon={item.icon}
+              content={item.content}
+              title={item.title}
+            />
+          ))}
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
 export default FeatureSection;
 
-function WalletIcon() {
+export function WalletIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -68,7 +67,7 @@ function WalletIcon() {
   );
 }
 
-function DollarIcon() {
+export function DollarIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -87,7 +86,7 @@ function DollarIcon() {
   );
 }
 
-function RefundIcon() {
+export function RefundIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -103,5 +102,17 @@ function RefundIcon() {
         d="M8.25 9.75h4.875a2.625 2.625 0 010 5.25H12M8.25 9.75L10.5 7.5M8.25 9.75L10.5 12m9-7.243V21.75l-3.75-1.5-3.75 1.5-3.75-1.5-3.75 1.5V4.757c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0c1.1.128 1.907 1.077 1.907 2.185z"
       />
     </svg>
+  );
+}
+
+export function FeatureCard({ icon, title, content }) {
+  return (
+    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md">
+      {icon}
+      <h3 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900">
+        {title}
+      </h3>
+      <p className="mb-3 font-normal text-gray-500">{content}</p>
+    </div>
   );
 }
