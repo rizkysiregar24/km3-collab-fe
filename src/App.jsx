66a66129d4a99b893e748f76f0e880f2ac9eaa68
praaannnn <fransxianiparz@gmail.com>
2022-12-08 14:51:34 +0700

    fix conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Protected from "./components/Routes/Protected";
+import { SearchResult } from "./pages/SearchResult";
+import { Notifications } from "./pages/Notifications";
 import Error from "./pages/Error";
 import { Home } from "./pages/Home";
 import Login from "./pages/Home/Login";
 import Register from "./pages/Home/Register";
 import User from "./pages/User";
+import Verifiedemail from "./pages/Authentication/Verifiedemail";
 import { ForgotPassword } from "./pages/ForgotPassword";
 import { ResetPassword } from "./pages/ResetPassword";
 
@@ -15,7 +18,6 @@ function App() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="/*" element={<Error />} />
-
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route
@@ -28,6 +30,9 @@ function App() {
         />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/search" element={<SearchResult />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/verified-email" element={<Verifiedemail />} />
       </Routes>
     </Router>
   );
