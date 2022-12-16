af776fb0c1e99ef2ca49fbaef90ea9033426f0a2
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-12-16 14:56:37 +0800

    feat: add button and inputdate components

---
+++ src/components/Input/Button.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+/**
+ *
+ * @param {React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>} props
+ */
+function Button(props) {
+  const { children, className, ...rest } = props;
+
+  return (
+    <button
+      className={`w-full font-semibold btn bg-brand hover:bg-brand-darker-800 ${className}`}
+      type="button"
+      {...rest}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
---
+++ src/components/Input/InputDate.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import CalendarIcon from '../Icons/CalendarIcon';
+
+/**
+ *
+ * @param {React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>} props
+ */
+function InputDate(props) {
+  return (
+    <div className="relative">
+      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <CalendarIcon />
+      </div>
+      <input
+        type="date"
+        className="input input-primary rounded-[4px] border-[#cccccc] border text-gray-900 text-sm w-full pl-10 p-2.5 appearance-none inline-flex items-center cursor-pointer"
+        {...props}
+      />
+    </div>
+  );
+}
+
+export default InputDate;
