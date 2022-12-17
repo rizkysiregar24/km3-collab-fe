8f9df065d4f35e4226ff1f625a01125f10d42a09
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-12-17 11:59:38 +0800

    feat: add modal component and usage example

--- src/pages/Admin page/Pageadmin.jsx
+++ src/pages/Admin page/Pageadmin.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React from 'react';
 
-import { Dashboard } from "../../components/Layout";
+import { Dashboard } from '../../components/Layout';
+// import CustomModal from '../../components/Modal/CustomModal';
 
 export default function Pageadmin() {
+  // const [isOpen, setIsOpen] = useState(false);
+
+  // const openModal = () => {
+  //   setIsOpen(true);
+  // };
+
+  // const closeModal = () => {
+  //   setIsOpen(false);
+  // };
+
   return (
     <Dashboard>
       <h1 className="font-bold mx-4 text-4xl lg:text-5xl text-transparent bg-clip-text bg-gradient-to-tr from-[#0b6cff] to-[#512bd4] p-2 text-center py-52">
         Welcome To Dashboard Admin
       </h1>
+      {/* <button onClick={openModal} type="button">
+        Buka modal
+      </button>
+      <CustomModal isOpen={isOpen} closeModal={closeModal}>
+        <h1>Hello from modal</h1>
+      </CustomModal> */}
     </Dashboard>
   );
 }
