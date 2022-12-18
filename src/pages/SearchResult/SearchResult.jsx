d3c8789f14038718a06472762d548435469b85ef
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-12-18 14:45:37 +0800

    fix(search): hide sort options when data not found

--- src/pages/SearchResult/SearchResult.jsx
+++ src/pages/SearchResult/SearchResult.jsx
@@ -43,38 +43,42 @@ export function SearchResult() {
       <div className="w-full mx-auto">
         <div className="flex justify-center my-8">
           <div className="flex justify-center items-center max-w-3xl flex-col gap-4 mx-4">
-            <div className="flex justify-between w-full">
-              <p>Showing {resultData?.length} results</p>
-              <label htmlFor="sort">
-                <select
-                  name="sort"
-                  id="sort"
-                  defaultValue="best"
-                  className="select select-bordered w-full max-w-xs select-xs">
-                  <option value="best">Best</option>
-                  <option value="price">Price</option>
-                  <option value="time">Time</option>
-                </select>
-              </label>
-            </div>
-            {resultData
-              ? resultData?.map((ticket) => (
-                  <ExpediaCard
-                    key={ticket.id}
-                    id={ticket.id}
-                    airline={ticket.name}
-                    departureAirport={ticket.departureAirport}
-                    departureIata={ticket.departure}
-                    arrivalAirport={ticket.arrivalAirport}
-                    arrivalIata={ticket.arrival}
-                    departureTime={ticket.departureTime}
-                    arrivalTime={ticket.arrivalTime}
-                    price={ticket.price}
-                    sc={ticket.sc}
-                    tripType={ticket.tripType.split('_').join(' ')}
-                  />
-                ))
-              : 'No flights found for this route at this date'}
+            {resultData ? (
+              <div className="flex justify-between w-full">
+                <p>Showing {resultData?.length} results</p>
+                <label htmlFor="sort">
+                  <select
+                    name="sort"
+                    id="sort"
+                    defaultValue="best"
+                    className="select select-bordered w-full max-w-xs select-xs">
+                    <option value="best">Best</option>
+                    <option value="price">Price</option>
+                    <option value="time">Time</option>
+                  </select>
+                </label>
+              </div>
+            ) : null}
+            {resultData ? (
+              resultData?.map((ticket) => (
+                <ExpediaCard
+                  key={ticket.id}
+                  id={ticket.id}
+                  airline={ticket.name}
+                  departureAirport={ticket.departureAirport}
+                  departureIata={ticket.departure}
+                  arrivalAirport={ticket.arrivalAirport}
+                  arrivalIata={ticket.arrival}
+                  departureTime={ticket.departureTime}
+                  arrivalTime={ticket.arrivalTime}
+                  price={ticket.price}
+                  sc={ticket.sc}
+                  tripType={ticket.tripType.split('_').join(' ')}
+                />
+              ))
+            ) : (
+              <div className="h-96">Loading....</div>
+            )}
           </div>
         </div>
       </div>
