33ef8f8c081cd4f8d05276a1986015a39f57e31b
Muhammad Rizky Pratama Siregar <mrizkypratamasiregar24@gmail.com>
2022-12-19 21:29:26 +0700

    feture_baru_trasaction

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -14,13 +14,7 @@ function Navbar() {
 
   const isAdmin = userDataRedux.role === "Admin";
 
-  const isValidUser =
-    Boolean(userData?.username) &&
-    Boolean(userData?.email) &&
-    Boolean(userData?.role) &&
-    Boolean(userDataRedux?.name) &&
-    Boolean(userDataRedux?.email) &&
-    Boolean(userDataRedux?.role);
+  const isValidUser = Boolean(userData?.username) && Boolean(userData?.email) && Boolean(userData?.role) && Boolean(userDataRedux?.name) && Boolean(userDataRedux?.email) && Boolean(userDataRedux?.role);
 
   const dispatch = useDispatch();
 
@@ -32,40 +26,23 @@ function Navbar() {
     <nav className="bg-white text-black shadow-md ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <Link
-            className="font-bold text-2xl hidden md:inline-flex items-center gap-2"
-            to="/"
-            title="Back to Home"
-          >
+          <Link className="font-bold text-2xl hidden md:inline-flex items-center gap-2" to="/" title="Back to Home">
             <Logo size={36} />
             Terbang Tinggi
           </Link>
-          <Link
-            className="font-bold text-2xl md:hidden inline-flex items-center gap-2"
-            to="/"
-          >
+          <Link className="font-bold text-2xl md:hidden inline-flex items-center gap-2" to="/">
             <Logo />
           </Link>
           <div className="hidden md:block">
             {isValidUser ? (
-              <AuthRightElementNavbar
-                handleLogout={handleLogout}
-                isAdmin={isAdmin}
-                username={userDataRedux.name}
-              />
+              <AuthRightElementNavbar handleLogout={handleLogout} isAdmin={isAdmin} username={userDataRedux.name} />
             ) : (
               <div className="ml-10 flex items-baseline space-x-4">
-                <Link
-                  to="/login"
-                  className=" hover:bg-brand-darker-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium border-brand border-2"
-                >
+                <Link to="/login" className=" hover:bg-brand-darker-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium border-brand border-2">
                   Login
                 </Link>
 
-                <Link
-                  to="/register"
-                  className="hover:bg-brand-darker-800  px-3 py-2 rounded-md text-sm font-medium bg-brand text-white border-brand border-solid border-2"
-                >
+                <Link to="/register" className="hover:bg-brand-darker-800  px-3 py-2 rounded-md text-sm font-medium bg-brand text-white border-brand border-solid border-2">
                   Register
                 </Link>
               </div>
@@ -75,11 +52,7 @@ function Navbar() {
           {/* Show or hide hamburger button on mobile */}
           {isValidUser ? (
             <div className="md:hidden">
-              <AuthRightElementNavbar
-                handleLogout={handleLogout}
-                isAdmin={isAdmin}
-                username={userDataRedux.name}
-              />
+              <AuthRightElementNavbar handleLogout={handleLogout} isAdmin={isAdmin} username={userDataRedux.name} />
             </div>
           ) : (
             <div className="-mr-2 flex md:hidden">
@@ -92,36 +65,12 @@ function Navbar() {
               >
                 <span className="sr-only">Open main menu</span>
                 {!isOpen ? (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
+                  <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                   </svg>
                 ) : (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
+                  <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                   </svg>
                 )}
               </button>
@@ -135,17 +84,11 @@ function Navbar() {
         <div className={isOpen ? "block" : "hidden"}>
           <div className="md:hidden" id="mobile-menu">
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-              <Link
-                to="/login"
-                className="hover:bg-gray-200 block px-3 py-2 rounded-md text-base font-medium"
-              >
+              <Link to="/login" className="hover:bg-gray-200 block px-3 py-2 rounded-md text-base font-medium">
                 Login
               </Link>
 
-              <Link
-                to="/register"
-                className=" hover:bg-gray-200 block px-3 py-2 rounded-md text-base font-medium"
-              >
+              <Link to="/register" className=" hover:bg-gray-200 block px-3 py-2 rounded-md text-base font-medium">
                 Register
               </Link>
             </div>
@@ -162,19 +105,9 @@ export function AuthRightElementNavbar({ handleLogout, isAdmin, username }) {
   return (
     <div className="flex items-center gap-2">
       <Link to="/notifications">
-        <button
-          className="btn btn-ghost btn-circle"
-          type="button"
-          title="Notifications"
-        >
+        <button className="btn btn-ghost btn-circle" type="button" title="Notifications">
           <div className="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -188,21 +121,10 @@ export function AuthRightElementNavbar({ handleLogout, isAdmin, username }) {
         </button>
       </Link>
       <div className="dropdown dropdown-end">
-        <label
-          tabIndex={0}
-          className="btn btn-outline btn-primary btn-sm btn-circle avatar placeholder"
-          title="Profile Menu"
-        >
-          {username ? (
-            <span>{username[0].toUpperCase()}</span>
-          ) : (
-            <span>TT</span>
-          )}
+        <label tabIndex={0} className="btn btn-outline btn-primary btn-sm btn-circle avatar placeholder" title="Profile Menu">
+          {username ? <span>{username[0].toUpperCase()}</span> : <span>TT</span>}
         </label>
-        <ul
-          tabIndex={0}
-          className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-        >
+        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
           {isAdmin ? (
             <li>
               <Link to="/admin-page">Dashboard</Link>
@@ -212,7 +134,7 @@ export function AuthRightElementNavbar({ handleLogout, isAdmin, username }) {
             <Link to="/user">Profile</Link>
           </li>
           <li>
-            <Link to="/transactions">Transactions</Link>
+            <Link to="/transaction">Transactions</Link>
           </li>
           <li>
             <button type="button" onClick={handleLogout}>
