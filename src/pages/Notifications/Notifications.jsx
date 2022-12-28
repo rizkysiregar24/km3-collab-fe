78e1701eaf5f35563d8694ab548aa1579bb168b2
praaannnn <fransxianiparz@gmail.com>
2022-12-28 20:27:12 +0700

    notification

--- src/pages/Notifications/Notifications.jsx
+++ src/pages/Notifications/Notifications.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 
 import axios from 'axios';
 import { toast } from 'react-toastify';
-import { Layout } from '../../components/Layout';
+import Navbar from '../../components/Layout/Navbar';
+
+// import { Layout } from '../../components/Layout';
 
 export function Notifications() {
   const [notif, setNotif] = useState([]);
@@ -19,8 +21,7 @@ export function Notifications() {
 
     axios(config)
       .then((response) => {
-        setNotif(response.data);
-        toast(response.data.message);
+        setNotif(response.data.data);
       })
       .catch((error) => {
         toast(error);
@@ -28,9 +29,31 @@ export function Notifications() {
   }, []);
 
   return (
-    <Layout>
-      <div className="h-screen">Notifications Page</div>
-      <div>{notif.id}</div>
-    </Layout>
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div className="h-screen">
+        <div className="text-2xl font-bold flex mt-5 ml-28">
+          Notifications
+          <div className="badge badge-xs badge-primary indicator-item" />
+        </div>
+        {notif?.map((x) => (
+          <div className="mx-28 bg-sky-50 rounded-lg mt-5 drop-shadow-md hover:drop-shadow-xl cursor-pointer">
+            <div key={x} className="mx-5 ">
+              <div className="font-bold text-xl text-blue-500 py-1 font-sans">#{x.id}</div>
+              <div className="flex">
+                <div className="font-bold text-lg font-sans">{x.detail_message}</div>
+                <div className="ml-2 text-sm italic mt-1 font-sans">{x.title}</div>
+              </div>
+              <div className="py-2 text-slate-400  font-sans">
+                <div>created : {x.createdAt}</div>
+                <div>updated : {x.updatedAt}</div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
