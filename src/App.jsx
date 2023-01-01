825c5369842e99ca9c1f80683b72576ee3d97223
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2023-01-01 16:58:11 +0800

    refactor: extract routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,9 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import Protected from './components/Routes/Protected';
-import { SearchResult } from './pages/SearchResult';
-import { Notifications } from './pages/Notifications';
-import Error from './pages/Error';
 import { Home } from './pages/Home';
-import { Login } from './pages/Login';
-import { Register } from './pages/Register';
 import User from './pages/Profile/User';
-import Verifiedemail from './pages/Authentication/Verifiedemail';
-import { ForgotPassword } from './pages/ForgotPassword';
-import { ResetPassword } from './pages/ResetPassword';
-import Userpage from './pages/Admin page/Userpage';
-import Pageadmin from './pages/Admin page/Pageadmin';
-import Transaction from './pages/transaction page/Transaction';
-import CreateTicket from './pages/Admin page/Ticket/CreateTicket';
-import ListTicket from './pages/Admin page/Ticket/ListTicket';
-import UpdateTicket from './pages/Admin page/Ticket/UpdateTicket';
-import { Booking } from './pages/Booking';
-import Payment from './pages/Booking/Payment';
-import { History } from './pages/History';
-import Detail from './pages/detail/Detail';
-import Cart from './pages/Booking/Cart';
-import ETicket from './pages/Booking/ETicket';
-import { Dashboard } from './pages/Dashboard';
-import { Reset } from './pages/Reset';
-import { AboutUs } from './pages/AboutUs';
+import routes from './routes';
 
 function App() {
   return (
@@ -36,9 +14,6 @@ function App() {
       <Router>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/*" element={<Error />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
           <Route
             path="/user"
             element={
@@ -47,27 +22,9 @@ function App() {
               </Protected>
             }
           />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/search" element={<SearchResult />} />
-          <Route path="/notifications" element={<Notifications />} />
-          <Route path="/verified-email" element={<Verifiedemail />} />
-          <Route path="/transaction" element={<Transaction />} />
-          <Route path="/admin-page" element={<Pageadmin />} />
-          <Route path="/user-page" element={<Userpage />} />
-          <Route path="/create-ticket" element={<CreateTicket />} />
-          <Route path="/ticket" element={<ListTicket />} />
-          <Route path="/ticket/:id" element={<UpdateTicket />} />
-          <Route path="/booking/:id" element={<Booking />} />
-          <Route path="/payment/:paymentId" element={<Payment />} />
-          <Route path="/history" element={<History />} />
-          <Route path="/detail-user/:id" element={<Detail />} />
-          <Route path="/eticket/:paymentId" element={<ETicket />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/about" element={<AboutUs />} />
-
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/reset" element={<Reset />} />
+          {routes.map((route) => (
+            <Route key={route.path} path={route.path} element={<route.component />} />
+          ))}
         </Routes>
       </Router>
       <ToastContainer autoClose={500} />
---
+++ src/routes/index.js
@@ -0,0 +1,120 @@
+import { AboutUs } from '../pages/AboutUs';
+import { Booking } from '../pages/Booking';
+import Cart from '../pages/Booking/Cart';
+import CreateTicket from '../pages/Admin page/Ticket/CreateTicket';
+import Detail from '../pages/detail/Detail';
+import Error from '../pages/Error';
+import ETicket from '../pages/Booking/ETicket';
+import { ForgotPassword } from '../pages/ForgotPassword';
+import { History } from '../pages/History';
+import ListTicket from '../pages/Admin page/Ticket/ListTicket';
+import { Login } from '../pages/Login';
+import { Notifications } from '../pages/Notifications';
+import Pageadmin from '../pages/Admin page/Pageadmin';
+import Payment from '../pages/Booking/Payment';
+import { PrivacyPolicy } from '../pages/PrivacyPolicy';
+import { Register } from '../pages/Register';
+import { Reset } from '../pages/Reset';
+import { ResetPassword } from '../pages/ResetPassword';
+import { SearchResult } from '../pages/SearchResult';
+import Transaction from '../pages/transaction page/Transaction';
+import UpdateTicket from '../pages/Admin page/Ticket/UpdateTicket';
+import Userpage from '../pages/Admin page/Userpage';
+import Verifiedemail from '../pages/Authentication/Verifiedemail';
+
+const routes = [
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/register',
+    component: Register
+  },
+  {
+    path: '/forgot-password',
+    component: ForgotPassword
+  },
+  {
+    path: '/reset-password',
+    component: ResetPassword
+  },
+  {
+    path: '/search',
+    component: SearchResult
+  },
+  {
+    path: '/notifications',
+    component: Notifications
+  },
+  {
+    path: '/verified-email',
+    component: Verifiedemail
+  },
+  {
+    path: '/transactions',
+    component: Transaction
+  },
+  {
+    path: '/admin-page',
+    component: Pageadmin
+  },
+  {
+    path: '/user-page',
+    component: Userpage
+  },
+  {
+    path: '/create-flight',
+    component: CreateTicket
+  },
+  {
+    path: '/flights',
+    component: ListTicket
+  },
+  {
+    path: '/flights/:id',
+    component: UpdateTicket
+  },
+  {
+    path: '/booking/:id',
+    component: Booking
+  },
+  {
+    path: '/payment/:paymentId',
+    component: Payment
+  },
+  {
+    path: '/history',
+    component: History
+  },
+  {
+    path: '/detail-user/:id',
+    component: Detail
+  },
+  {
+    path: '/eticket/:paymentId',
+    component: ETicket
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/about',
+    component: AboutUs
+  },
+  {
+    path: '/policy',
+    component: PrivacyPolicy
+  },
+  {
+    path: '/reset',
+    component: Reset
+  },
+  {
+    path: '/*',
+    component: Error
+  }
+];
+
+export default routes;
